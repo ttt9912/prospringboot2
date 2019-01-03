fc5060efadd1eca7506c3d66146b6da717a67bd4
ttt <tschachtli.th@gmail.com>
2019-01-03 18:15:35 +0100

    ch3

--- ch3-springboot-features/app-configuration/properties-profile-based/src/main/java/ch3/properties_profiles/ProfileBasedDemo.java
+++ ch3-springboot-features/app-configuration/properties-profile-based/src/main/java/ch3/properties_profiles/ProfileBasedDemo.java
@@ -2,8 +2,14 @@ package ch3.properties_profiles;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 /*
  * Create custom Properties and Beans based on Profiles
@@ -13,7 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - @ActiveProfile("dev")
  * - Environment.setActiveProfiles()
  * - SPRING_PROFILES_ACTIVE environment variable
- * - pring.profiles.active property
+ * - spring.profiles.active property
  *
  * # without active Profile
  *   spring uses the default - grabs application.properties
@@ -26,8 +32,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProfileBasedDemo {
     private static final Logger log = LoggerFactory.getLogger(ProfileBasedDemo.class);
 
+    // > mvn spring-boot:run -Dspring.profiles.active=prod
     public static void main(String[] args) {
         SpringApplication.run(ProfileBasedDemo.class, args)
                 .close();
+    }
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            log.info("# active profiles={}", Arrays.asList(env.getActiveProfiles()));
+            log.info("# server.ip={}", env.getProperty("server.ip"));
+        };
     }
 }
