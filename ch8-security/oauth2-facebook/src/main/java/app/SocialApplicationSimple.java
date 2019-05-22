a7637d30cae19061e4869a0aa8810c0eb9ddaf24
ttt <tschachtli.th@gmail.com>
2019-05-22 23:26:50 +0200

    simple oauth2 sso facebook

---
+++ ch8-security/oauth2-facebook/src/main/java/app/SocialApplicationSimple.java
@@ -0,0 +1,32 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+/*
+ * https://spring.io/guides/tutorials/spring-boot-oauth2/#_social_login_simple
+ *
+ * ---------------------------------------------------------------------------------
+ * Register app on Facebook
+ * ---------------------------------------------------------------------------------
+ * 1. Create a new App on Facebook for Developers
+ * 2. App -> Settings -> Basic
+ *    - copy paste App ID & App Secret to application.yml
+ *
+ * ---------------------------------------------------------------------------------
+ * run
+ * ---------------------------------------------------------------------------------
+ * 1. go to localhost:8080
+ * 2. will be redirected to Facebook to login with Facebook account
+ * 3. after login, redirected to localhost:8080
+ *
+ */
+@SpringBootApplication
+@EnableOAuth2Sso // make the link to Facebook, delegate login to Facebook (OAuth2)
+public class SocialApplicationSimple {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplicationSimple.class, args);
+    }
+
+}
