74cadc79a8e93ab81d1d32fce0d2e24611423b30
ttt <tschachtli.th@gmail.com>
2019-12-01 11:49:54 +0100

    blabalbalbal

--- ch8-security/keycloak-sso/keycloak-spring-security/src/main/java/keycloaksso/springsecurity/KeycloakSecurityApp.java
+++ ch8-security/keycloak-sso/keycloak-spring-security/src/main/java/keycloaksso/springsecurity/KeycloakSecurityApp.java
@@ -7,6 +7,15 @@ import org.springframework.context.annotation.Import;
 
 /*
  * --------------------------------------------------------
+ * TODO: Problem
+ * --------------------------------------------------------
+ * - /api/* is secured
+ * - call to localhost:8080/api/edutasks will redirect to keycloak logn (good)
+ * - call to localhost:8080 will load angular, which will call localhost:8080/api/edutasks
+ *   but no redirect will happen, just angular page without results (bad)
+ * - Angular does not follow the Location in the response header
+ *
+ * --------------------------------------------------------
  * Edurion Angular Webjar
  * --------------------------------------------------------
  * - clean install edurion-ui-angular first
