3687be85402943fa4a309b1cfeab7d261db900a8
ttt <tschachtli.th@gmail.com>
2020-04-12 22:27:28 +0200

    sendtouser

--- ch9-messaging/websockets/websocketbroker-stomp-push-sendtouser/src/main/java/ch9/simpsendtouser/endpoint/ChatController.java
+++ ch9-messaging/websockets/websocketbroker-stomp-push-sendtouser/src/main/java/ch9/simpsendtouser/endpoint/ChatController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import java.security.Principal;
 
 /*
- * TODO
  * -------------------------------------------------
  * /queue
  * -------------------------------------------------
