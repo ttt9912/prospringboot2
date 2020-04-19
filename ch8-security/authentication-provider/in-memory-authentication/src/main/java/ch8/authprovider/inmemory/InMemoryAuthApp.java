34b298861f50cd97c068926bb43ec91936744178
ttt <tschachtli.th@gmail.com>
2020-04-19 23:56:05 +0200

    security

--- ch8-security/authentication-provider/in-memory-authentication/src/main/java/ch8/authprovider/inmemory/InMemoryAuthApp.java
+++ ch8-security/authentication-provider/in-memory-authentication/src/main/java/ch8/authprovider/inmemory/InMemoryAuthApp.java
@@ -5,6 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+/*
+ *
+ * go to localhost:8080/api/todos
+ *
+ * Logins
+ * - user:password
+ * - admin:password
+ */
 @SpringBootApplication
 @Import(CommonTodoRestConfig.class)
 public class InMemoryAuthApp {
--- ch8-security/authentication-provider/in-memory-authentication/src/main/java/ch8/authprovider/inmemory/SecurityConfig.java
+++ ch8-security/authentication-provider/in-memory-authentication/src/main/java/ch8/authprovider/inmemory/SecurityConfig.java
@@ -1,9 +1,15 @@
 package ch8.authprovider.inmemory;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
+/*
+ * Provide a bean of UserDetailsService
+ */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -16,4 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().fullyAuthenticated().and()
                 .formLogin();
     }
+
+    /*
+     * inMemoryAuthentication() - provides a bean of type InMemoryUserDetailsManager
+     * which implements UserDetailsService
+     */
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+
+        auth.inMemoryAuthentication()
+                .withUser(user).withUser(admin);
+    }
+
 }
