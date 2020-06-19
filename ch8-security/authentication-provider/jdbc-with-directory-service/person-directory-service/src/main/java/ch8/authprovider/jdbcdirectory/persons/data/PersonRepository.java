21d29845173a67911a66f23b711830ae4adb243e
ttt <tschachtli.th@gmail.com>
2020-06-19 13:58:40 +0200

    aaaaa

--- ch8-security/authentication-provider/jdbc-with-directory-service/person-directory-service/src/main/java/ch8/authprovider/jdbcdirectory/persons/data/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package ch8.authprovider.jdbcdirectory.persons.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface PersonRepository extends CrudRepository<Person, String> {
-    Person findByEmailIgnoreCase(@Param("email") String email);
-}
